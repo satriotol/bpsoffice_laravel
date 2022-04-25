180efc09a5ba412c1c705b30635ed5b8996e421c
SATRIO <satriotol69@gmail.com>
2022-04-25 13:58:03 +0700

    stisla

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [IndexController::class, 'home'])->name('home');
+Route::get('/admin', [AdminController::class, 'admin'])->name('admin');
