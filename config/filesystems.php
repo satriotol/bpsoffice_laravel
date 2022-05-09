26bcc6eb0c540f0a15f079fafb42e2fd37fa3ffb
SATRIO <satriotol69@gmail.com>
2022-05-09 14:06:05 +0700

    REVERT

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('STORAGE_URL'),
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
